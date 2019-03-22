8d35786ea10425bac48b6f70f7e1611d1028da39
ronaarika86 <ronaarikahasna99@gmail.com>
2019-03-22 09:56:54 +0700

    rona

---
+++ rona/app/Http/Controllers/produkController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class produkController extends Controller
+{
+    public function index() { return 'Mengakses Fungsi di Controller menggunakan route';
+	}
+}
--- rona/routes/web.php
+++ rona/routes/web.php
@@ -13,4 +13,28 @@
 
 Route::get('/', function () {
     return view('welcome');
+	
 });
+Route::get('/welcome', function () {  
+echo "Welcome";  
+}); 
+Route::get('/show/{id}', function ($id) {
+	echo "Nilai Paramater Adalah ".$id;
+});
+
+Route::get('/show/{id?}', function ($id=1) {
+	echo "Nilai Paramater Adaalah ".$id;
+});
+
+Route::get('/edit/{nama}', function ($nama) {  
+echo "Nilai Parameter Adalah ".$nama;  })->where('nama','[A-Za-z]+'); 
+
+Route::get('/index', function () {  echo "<a href='".route('create')."'>Akses Route dengan nama </a>";  
+}); 
+
+Route::get('/produk', 'produkController@index');
+
+
+Route::get('/create', function () {  
+echo "Route diakses menggunakan nama"; 
+})->name('create');
